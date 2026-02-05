4babdf03187569f743f4777696d6a46d2f9b3e89
shahbaz123-gif <sajjadahmadjan30@gmail.com>
2026-02-05 21:40:26 +0500

    Add files via upload

---
+++ database.py
@@ -0,0 +1,8 @@
+print("what is the type of above given database - \n 1)relational database \n 2)non-relational database")
+answer = int(input("enter your guess here..."))
+if answer == 2:
+    print("you guessed it right!")
+else:
+    print("unfortunately your guess is wrong.")    
+
+print("\nplease tell your mentor why you guessed this?")
